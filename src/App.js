37cb6015f634a60227713a243e3926a03048aaea
likaterina <linainadelira@gmail.com>
2019-12-22 20:31:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default function BasicExample() {
+  const [user, setUser] = useState({})
+
+  useEffect(() => {
+    fetch('localhost:3228/auth/get-current-user')
+      .then(res => res.json())
+      
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Login</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Chat</Link>
+          </li>
+        </ul>
+
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Enter your login and password</h2>
     </div>
   );
 }
 
-export default App;
+function Dashboard() {
+  return (
+    <div>
+      <h2>Let`s talk</h2>
+    </div>
+  );
+}
+
